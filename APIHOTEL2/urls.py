1a7a36b75aa68604ff1e224fb9744746e8298947
SPECTER1807 <vicmar0718@gmail.com>
2023-11-22 15:17:26 -0600

    Api Bien

---
+++ APIHOTEL2/urls.py
@@ -0,0 +1,53 @@
+"""APIHOTEL2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from api.views import *
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index.html',Home.as_view(),name='index'),
+    path('about.html',About.as_view(),name='about'),
+    path('amenities.html',Amenities.as_view(),name='amenities'),
+    path('bookings.html',Bookings.as_view(),name='bookings'),
+    path('contact.html',Contact.as_view(),name='contact'),
+    path('failed.html',Failed.as_view(),name='failed'),
+    path('faqs.html',Faqs.as_view(),name='faqs'),
+    path('gallery.html',Gallery.as_view(),name='gallery'),
+    path('privacy-policy.html',Privacy.as_view(),name='privacy'),
+    path('rooms.html',Rooms.as_view(),name='rooms'),
+    path('alcove.html',Alcove.as_view(),name='alcove'),
+    path('cottage.html',Cottage.as_view(),name='cottage'),
+    path('business-suite.html',Business.as_view(),name='business'),
+    path('deluxe.html',Deluxe.as_view(),name='deluxe'),
+    path('executive.html',Executive.as_view(),name='executive'),
+    path('round-house.html',RoundHouse.as_view(),name='roundhouse'),
+    path('studio.html',Studio.as_view(),name='studio'),
+    path('vip.html',Vip.as_view(),name='vip'),
+    path('vvip.html',Vvip.as_view(),name='vvip'),
+    path('success.html',Success.as_view(),name='success'),
+    
+    # path('checkout/<int:product_id>/', views.CheckOut, name='checkout'),
+    # path('payment-successful/<int:product_id>/', views.PaymentSuccessful, name='payment-successful'),
+    # path('payment-failed/<int:product_id>/', views.PaymentFailed, name='payment-failed'),
+    
+    #  path('reservar-habitacion/', reservar_habitacion, name='reservar_habitacion'),
+
+    
+]
