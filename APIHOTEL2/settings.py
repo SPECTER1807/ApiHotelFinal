261590de16083092ab61a8cf59432a42b4c5e0ce
SPECTER1807 <vicmar0718@gmail.com>
2023-11-27 15:41:03 -0600

    gunicorn

--- APIHOTEL2/settings.py
+++ APIHOTEL2/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import environ
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
