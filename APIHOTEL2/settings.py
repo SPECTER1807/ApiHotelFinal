a2ed31dd0007335be267a37f768e5b462241c602
SPECTER1807 <vicmar0718@gmail.com>
2023-11-27 15:54:56 -0600

    requiremets

--- APIHOTEL2/settings.py
+++ APIHOTEL2/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
-import environ
+
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
