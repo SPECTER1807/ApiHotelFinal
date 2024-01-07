7b603480360af15566ce92b832ed00fe9b39c743
SPECTER1807 <vicmar0718@gmail.com>
2024-01-07 17:49:13 -0600

    dash

--- api/views.py
+++ api/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 
 # Create your views here.
+
+class Success(APIView):
+    template_name="dashboard.html"
+    def get(self, request):
+        return render(request,self.template_name) 
+    
 class Home(APIView):
     template_name="index.html"
     def get(self, request):
